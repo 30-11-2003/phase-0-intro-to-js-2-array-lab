76b4386850f10e131a8c516037f0d56961024f9e
ShalomOrangi <145541724+30-11-2003@users.noreply.github.com>
2023-10-05 12:24:39 +0300

    Update arrays-lab

--- index.js
+++ index.js
@@ -1,1 +1,33 @@
 // Write your solution here!
+const cats =[]
+cats. push("Milo","Otis", "Garfield")
+console.log(cats)
+
+cats.push("Ralph")
+console.log(cats)
+
+cats.unshift("Bob")
+console.log(cats)
+
+cats.shift("Bob")
+cats.splice(2,3,)
+console.log(cats)
+
+cats.shift("milo")
+cats.push("Garfield")
+console.log(cats)
+
+cats.unshift("Milo")
+const Broom=[...cats,"Broom"]
+console.log(cats)
+console.log(Broom)
+
+const removeLastCat=[...cats,]
+removeLastCat.pop("Garfield")
+console.log(removeLastCat)
+console.log (cats)
+
+const removeFirstCat=[...cats]
+removeFirstCat.shift("Milo")
+console.log(removeFirstCat)
+console.log (cats)
